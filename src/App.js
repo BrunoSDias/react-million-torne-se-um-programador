b87c7472603e9a70bf01615cf40924f3b611351f
brunosdias <brunosdias@outlook.com>
2020-09-18 08:31:52 -0300

    Introduçao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Teste from './Teste';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  mostraMensagem = () => {
+    return (
+      <div>
+        <h4>dsdsdsd</h4>dsdsdsd
+        <h1> Aula tonre-se </h1>
+      </div>
+    )
+  }
+
+  exibirMensagem = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Teste></Teste>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Aprendendo React na comunidade do torne-se um programador
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Teste.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Teste extends Component{
+  render() {
+    return (
+      <div className="Teste">
+        <h1> Aula torne-se componente React</h1>
+      </div>
+    );
+  }
+}
+
+export default Teste;
